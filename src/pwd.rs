be9cf95694d2b5549515f7fd0625d942d17a95d8
arch machine <lilith.alanis.255@gmail.com>
2025-05-10 19:14:36 -0600

    man why

--- src/pwd.rs
+++ src/pwd.rs
@@ -1,11 +1,12 @@
 use std::{ffi::CStr, path::PathBuf};
 
 use libc::{geteuid, getgid, getpwuid};
+use thiserror::Error;
 
 use crate::error::{LibcError, LibcResult};
 
 #[derive(Debug, Default)]
-pub struct Passwd{
+pub struct Passwd {
     pub name: String,
     pub passwd: String, /* unimplemented */
     pub uid: u32,
@@ -15,14 +16,16 @@ pub struct Passwd{
     pub shell: String, /* unimplemented */
 }
 
-pub fn get_passwd() -> LibcResult<Passwd>{
-    unsafe{
+#[derive(Error, Clone, Debug)]
+enum PWDError{
+}
+
+pub fn get_passwd() -> LibcResult<Passwd> {
+    unsafe {
         let uid = geteuid();
         let gid = getgid();
         let pwd = getpwuid(uid);
-        if pwd.is_null() {
-            return Err(LibcError::ReturnedNull(format!("getpwuid({uid})")))
-        }
+        if pwd.is_null() { }
 
         let name = CStr::from_ptr((*pwd).pw_name).to_str()?.to_string();
         let dir = CStr::from_ptr((*pwd).pw_dir).to_str()?.to_string();
